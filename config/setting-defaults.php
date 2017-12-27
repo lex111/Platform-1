c43ac2a0f0f6285de0e662f5ec11324a588b161e
Yoginth <yoginth@aol.com>
2017-12-27 12:50:47 +0000

    Updated Default Settings

--- config/setting-defaults.php
+++ config/setting-defaults.php
@@ -5,13 +5,13 @@
  */
 return [
 
-    'app-name'        => 'DocsPen',
-    'app-logo' => '',
-    'app-name-header' => true,
-    'app-editor'      => 'wysiwyg',
-    'app-color'       => '#0079BF',
-    'app-color-light' => 'rgba(0, 121, 191, 0.15)',
-    'app-custom-head' => false,
-    'registration-enabled' => true,
+    'app-name'              => 'DocsPen',
+    'app-logo'              => 'https://unpkg.com/docspen@1.0.0/imgs/logo-small.png',
+    'app-name-header'       => true,
+    'app-editor'            => 'wysiwyg',
+    'app-color'             => '#0079BF',
+    'app-color-light'       => 'rgba(0, 121, 191, 0.15)',
+    'app-custom-head'       => false,
+    'registration-enabled'  => true,
 
 ];
