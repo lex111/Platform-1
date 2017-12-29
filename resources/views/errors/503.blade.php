2d23c8e323f49b98ab8ffdd5e4e7d0027439554f
Yoginth <yoginth@aol.com>
2017-12-29 20:02:46 +0530

    Update 503.blade.php

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -5,7 +5,7 @@
     <div class="container small">
         <div class="card" style="background-color:transparent;box-shadow:none">
             <div class="body">
-                <center><img src="https://unpkg.com/docspen@4.0.0/imgs/unicorn.png" style="height:12em;pointer-events:none"></center>
+                <center><img src="https://cdn.jsdelivr.net/npm/docspen@4.0.0/imgs/unicorn.png" style="height:12em;pointer-events:none"></center>
                 <center><h5><b>We're having a really bad day.</b></h5></center>
                 <center><p><h5 style="color:#797979">The Unicorns have taken over. We're doing our best to get them under control and get DocsPen back up and running.</h5></p></center>
             </div>
