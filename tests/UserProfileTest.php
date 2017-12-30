bfde71c8edf6808b1c87aaeef0e8ab8f47fd7746
Yoginth <yoginth@aol.com>
2017-12-30 10:53:27 +0000

    Added LICENSE

--- tests/UserProfileTest.php
+++ tests/UserProfileTest.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Copyright (c) 2017 - present, DocsPen, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 namespace Tests;
 
 class UserProfileTest extends BrowserKitTest
