4524b7367a9ce77ebfa42ba5a1fc70eabbca332a
Yoginth <yoginth@aol.com>
2017-12-30 10:50:42 +0000

    Apply fixes from StyleCI

--- database/migrations/2017_07_02_152834_update_db_encoding_to_ut8mb4.php
+++ database/migrations/2017_07_02_152834_update_db_encoding_to_ut8mb4.php
@@ -7,7 +7,6 @@
  * This source code is licensed under the license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
 use Illuminate\Database\Migrations\Migration;
 
 class UpdateDbEncodingToUt8mb4 extends Migration
