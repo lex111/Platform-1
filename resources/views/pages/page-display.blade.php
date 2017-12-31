bcd539930767f016943ae05ed8ad11a3965d7e94
Yoginth <yoginth@aol.com>
2017-12-31 08:45:11 +0000

    Added Pocket Button

--- resources/views/pages/page-display.blade.php
+++ resources/views/pages/page-display.blade.php
@@ -2,6 +2,7 @@
 
     <h1 id="bkmrk-page-title">{{$page->name}}</h1>
     <div style="clear:left"></div>
+    <a data-pocket-label="pocket" data-pocket-count="vertical" class="pocket-btn" data-lang="en"></a>
 
     @if (isset($diff) && $diff)
         {!! $diff !!}
@@ -9,3 +10,4 @@
         {!! isset($page->renderedHTML) ? $page->renderedHTML : $page->html !!}
     @endif
 </div>
+<script type="text/javascript">!function(d,i){if(!d.getElementById(i)){var j=d.createElement("script");j.id=i;j.src="https://widgets.getpocket.com/v1/j/btn.js?v=1";var w=d.getElementById(i);d.body.appendChild(j);}}(document,"pocket-btn-js");</script>
