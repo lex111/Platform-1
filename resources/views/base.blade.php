1001d282ed19bb0b64c832ccc1547d11cac14cdf
Yoginth <yoginth@aol.com>
2017-12-31 17:23:50 +0000

    Added SW

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -29,11 +29,16 @@
         <!-- End custom user content -->
     @endif
     <script>
-        if('serviceWorker' in navigator) {
-          navigator.serviceWorker.register('/serviceworker.js', {
-            scope: '/'
-          });
+        if ('serviceWorker' in navigator) {
+          navigator.serviceWorker.register('service-worker.js');
         }
+        
+        document.querySelector('#show').addEventListener('click', () => {
+          const iconUrl = document.querySelector('select').selectedOptions[0].value;
+          let imgElement = document.createElement('img');
+          imgElement.src = iconUrl;
+          document.querySelector('#container').appendChild(imgElement);
+        });
     </script>
 </head>
 <body class="@yield('body-class')">
