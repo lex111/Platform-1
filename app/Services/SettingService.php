7ab141a8d83722877799cb4814995dc146fce102
Yoginth <yoginth@aol.com>
2017-12-31 04:43:28 +0000

    Fixed failing book view test

--- app/Services/SettingService.php
+++ app/Services/SettingService.php
@@ -123,6 +123,9 @@ class SettingService
     {
         $cacheKey = $this->cachePrefix.$key;
         $this->cache->forget($cacheKey);
+        if (isset($this->localCache[$key])) {
+            unset($this->localCache[$key]);
+        }
     }
 
     /**
