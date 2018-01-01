3d6e8034b3527376f6750a06a3723dbae9f58178
Yoginth <yoginth@aol.com>
2018-01-01 09:02:06 +0000

    Implemented Users Sitemap

--- resources/views/sitemaps/users.blade.php
+++ resources/views/sitemaps/users.blade.php
@@ -2,9 +2,9 @@
 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
         xmlns:xhtml="http://www.w3.org/1999/xhtml">
-    @foreach($books as $book)
+    @foreach($users as $user)
         <url>
-            <loc>{{ $book->getUrl() }}</loc>
+            <loc>{{ baseUrl("/settings/users/{$user->id}") }}</loc>
             <changefreq>hourly</changefreq>
             <priority>0.8</priority>
         </url>
