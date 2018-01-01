a6c36d73344e549b88c894a362e0c9a590db1f48
Yoginth <yoginth@aol.com>
2018-01-01 08:23:17 +0000

    Implementing About Page

--- resources/views/others/about.blade.php
+++ resources/views/others/about.blade.php
@@ -2,6 +2,14 @@
 
 @section('content')
 
+	<title>About | DocsPen</title>
+	<meta property="og:type" content="article" />
+	<meta property="og:title" content="About | DocsPen" />
+	<meta property="og:url" content="{{ config('app.url') }}/about" />
+	<meta property="og:site_name" content="DocsPen" />
 
-
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:site" content="@votepen" />
+	<meta name="twitter:title" content="About | DocsPen" />
+	
 @stop
