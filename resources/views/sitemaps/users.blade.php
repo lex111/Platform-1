b4a82e3f42a39d9edbe609ea1676d4f3d388de99
Yoginth <yoginth@aol.com>
2018-01-01 09:10:30 +0000

    Implemented Users Sitemap

--- resources/views/sitemaps/users.blade.php
+++ resources/views/sitemaps/users.blade.php
@@ -2,5 +2,11 @@
 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
         xmlns:xhtml="http://www.w3.org/1999/xhtml">
-
+    @foreach($users as $user)
+        <url>
+            <loc>{{ $user->name }}</loc>
+            <changefreq>daily</changefreq>
+            <priority>0.7</priority>
+        </url>
+    @endforeach
 </urlset>
