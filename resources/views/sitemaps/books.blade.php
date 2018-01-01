a72925d66cd99747167075c2729502855d72c4c4
Yoginth <yoginth@aol.com>
2018-01-01 08:19:42 +0000

    Added SitemapController

--- resources/views/sitemaps/books.blade.php
+++ resources/views/sitemaps/books.blade.php
@@ -2,11 +2,5 @@
 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
         xmlns:xhtml="http://www.w3.org/1999/xhtml">
-    @foreach($books as $book)
-        <url>
-            <loc>https://docspen.ga/@include('books/list-item', ['book' => $book])</loc>
-            <changefreq>hourly</changefreq>
-            <priority>0.8</priority>
-        </url>
-    @endforeach
+
 </urlset>
