4a65521cc87f14b7e05d3f3ce31af4df2277ee17
Yoginth <yoginth@aol.com>
2018-01-01 08:21:19 +0000

    Added SitemapController

--- resources/views/sitemaps/books.blade.php
+++ resources/views/sitemaps/books.blade.php
@@ -2,5 +2,11 @@
 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
         xmlns:xhtml="http://www.w3.org/1999/xhtml">
-
+    @foreach($books as $book)
+        <url>
+            <loc>{{ $book->getUrl() }}</loc>
+            <changefreq>hourly</changefreq>
+            <priority>0.8</priority>
+        </url>
+    @endforeach
 </urlset>
