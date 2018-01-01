20aa832b9cd7dec94024d1318c746c20b1740500
Yoginth <yoginth@aol.com>
2018-01-01 02:58:42 +0000

    Updated SW

--- resources/views/public.blade.php
+++ resources/views/public.blade.php
@@ -23,8 +23,22 @@
         {!! setting('app-custom-head') !!}
     @endif
     <script>
-        if ('serviceWorker' in navigator) {
-          navigator.serviceWorker.register('service-worker.js');
+    	if ('serviceWorker' in navigator && navigator.userAgent.indexOf("Mobile") === -1) {
+    		navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+    			console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    		})
+    		.catch(function(err) {
+    			console.error('ServiceWorker registration failed: ', err);
+    		});
+    	} else if('serviceWorker' in navigator && navigator.userAgent.indexOf("Mobile") > -1){
+            navigator.serviceWorker.getRegistration().then(function(registration) {
+                var serviceWorkerUnregistered=false;
+                if(registration) {
+                    registration.unregister();
+                    serviceWorkerUnregistered=true;
+                }
+                serviceWorkerUnregistered && window.location.reload();
+            });
         }
     </script>
 </head>
