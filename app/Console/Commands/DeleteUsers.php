8177adf9f42f8e5b058d44f6b661833bdeba39a7
Yoginth <yoginth@aol.com>
2018-01-03 11:54:49 +0000

    Updated LICENSE

--- app/Console/Commands/DeleteUsers.php
+++ app/Console/Commands/DeleteUsers.php
@@ -1,13 +1,5 @@
 <?php
 
-/**
- * Copyright (c) 2017 - present, DocsPen, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 namespace DocsPen\Console\Commands;
 
 use DocsPen\Repos\UserRepo;
