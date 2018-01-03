388d3564e25fb401ce8c019ca7004cb4c7174a02
Yoginth <yoginth@aol.com>
2018-01-03 02:26:37 +0000

    Updated LICENSE

--- database/migrations/2015_07_12_190027_create_pages_table.php
+++ database/migrations/2015_07_12_190027_create_pages_table.php
@@ -1,12 +1,4 @@
 <?php
-
-/**
- * Copyright (c) 2017 - present, DocsPen, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the license found in the
- * LICENSE file in the root directory of this source tree.
- */
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
