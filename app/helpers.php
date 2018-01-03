41bf286d290308d5c4fdeffcf80bfbbe3f7d6ca9
Yoginth <yoginth@aol.com>
2018-01-03 15:08:22 +0000

    Updated Version Handling in assets

--- app/helpers.php
+++ app/helpers.php
@@ -17,7 +17,7 @@ function versioned_asset($file = '')
 
     if (is_null($version)) {
         $versionFile = base_path('version');
-        $version = trim(file_get_contents($versionFile));
+        $version = sha1_file(public_path($file));
     }
 
     $additional = '';
