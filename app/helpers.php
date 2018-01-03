b79ef4e29fa1e4c44c680f621888db6f29e17039
Yoginth <yoginth@aol.com>
2018-01-03 15:19:04 +0000

    Updated Version Handling in assets

--- app/helpers.php
+++ app/helpers.php
@@ -15,7 +15,7 @@ function hashed_asset($file = '')
 {
 
     $hashed_value = sha1_file(public_path($file));
-    $path = $file.'.'.$hashed_value;
+    $path = $hashed_value.'.'.$file;
 
     return baseUrl($path);
 }
