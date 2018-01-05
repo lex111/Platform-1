0c8a2cdd8294aac8db49bfbcd6d8c16797e4a174
Yoginth <yoginth@aol.com>
2018-01-05 13:38:03 +0000

    Added TOS

--- resources/views/others/terms.blade.php
+++ resources/views/others/terms.blade.php
@@ -14,20 +14,6 @@
     <div class="entity-item-snippet" style="font-size:14px;line-height:20px">
         <p class="text-muted">Google's Data Center First Video Embed!</p>
     </div>
-    <div class="row meta text-muted text-small">
-        <div class="col-md-6">
-            <p class="text-muted small">
-            Revision #1 <br> Created <span title="Thu, Jan 4, 2018 4:17 AM">1 day ago</span> by <a href="https://docspen.ga/@/3">Yoginth</a>
-            <br>
-            Updated <span title="Thu, Jan 4, 2018 4:30 AM">1 day ago</span> by <a ></a>ef="https://docspen.ga/@/3">Yoginth</a>
-            </p> </div>
-            <div class="col-md-6">
-            <a class="text-book" href="https://docspen.ga/books/yoginth"><i class="zmdi zmdi-book"></i>Yoginth</a>
-            <br>
-            <i class="zmdi zmdi-collection-bookmark"></i> Page is not in a chapter
-            </div>
-        </div>
-    </div>
 </div>
 </div>
 </div>
