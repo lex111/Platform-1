3d1d01986268dbc4ea5b31e47114dd97967439b3
Yoginth <yoginth@aol.com>
2018-01-05 13:33:10 +0000

    Added TOS

--- resources/views/others/terms.blade.php
+++ resources/views/others/terms.blade.php
@@ -1,43 +1,146 @@
-<div class="page {{$page->draft ? 'draft' : ''}} entity-list-item" data-entity-type="page" data-entity-id="{{$page->id}}">
-    <h4>
-        @if (isset($showPath) && $showPath)
-            <a href="{{ $page->book->getUrl() }}" class="text-book">
-                <i class="zmdi zmdi-book"></i>{{ $page->book->getShortName() }}
-            </a>
-            <span class="text-muted">&nbsp;&nbsp;&raquo;&nbsp;&nbsp;</span>
-            @if($page->chapter)
-                <a href="{{ $page->chapter->getUrl() }}" class="text-chapter">
-                    <i class="zmdi zmdi-collection-bookmark"></i>{{ $page->chapter->getShortName() }}
-                </a>
-                <span class="text-muted">&nbsp;&nbsp;&raquo;&nbsp;&nbsp;</span>
-            @endif
-        @endif
-        <a href="{{ $page->getUrl() }}" class="text-page entity-list-item-link"><i class="zmdi zmdi-file-text"></i><span class="entity-list-item-name">{{ $page->name }}</span></a>
-    </h4>
+@extends('simple-layout')
 
-    <div class="entity-item-snippet" style="font-size:14px;line-height:20px">
-        @if(isset($page->searchSnippet))
-            <p class="text-muted">{!! $page->searchSnippet !!}</p>
-        @else
-            <p class="text-muted">{{ $page->getExcerpt() }}</p>
-        @endif
-    </div>
+@section('content')
 
-    @if(isset($style) && $style === 'detailed')
-        <div class="row meta text-muted text-small">
-            <div class="col-md-6">
-                @include('partials.entity-meta', ['entity' => $page])
-            </div>
-            <div class="col-md-6">
-                <a class="text-book" href="{{ $page->book->getUrl() }}"><i class="zmdi zmdi-book"></i>{{ $page->book->getShortName(30) }}</a>
-                <br>
-                @if($page->chapter)
-                    <a class="text-chapter" href="{{ $page->chapter->getUrl() }}"><i class="zmdi zmdi-collection-bookmark"></i>{{ $page->chapter->getShortName(30) }}</a>
-                @else
-                    <i class="zmdi zmdi-collection-bookmark"></i> {{ trans('entities.pages_not_in_chapter') }}
-                @endif
-            </div>
-        </div>
-    @endif
-
+<div class="card">
+<h3>Recently Updated Pages</h3>
+<div class="entity-list detailed" ng-non-bindable="">
+<div class="page  entity-list-item" data-entity-type="page" data-entity-id="7">
+<h4>
+<a href="https://docspen.ga/books/yoginth/page/googles-data-center" class="text-page entity-list-item-link"><i class="zmdi zmdi-file-text"></i><span class="entity-list-item-name">Google's Data Center</span></a>
+</h4>
+<div class="entity-item-snippet" style="font-size:14px;line-height:20px">
+<p class="text-muted">Google's Data Center
+First Video Embed!</p>
 </div>
+<div class="row meta text-muted text-small">
+<div class="col-md-6">
+<p class="text-muted small">
+Revision #1 <br> Created <span title="Thu, Jan 4, 2018 4:17 AM">1 day ago</span> by <a href="https://docspen.ga/@/3">Yoginth</a>
+<br>
+Updated <span title="Thu, Jan 4, 2018 4:30 AM">1 day ago</span> by <a href="https://docspen.ga/@/3">Yoginth</a>
+</p> </div>
+<div class="col-md-6">
+<a class="text-book" href="https://docspen.ga/books/yoginth"><i class="zmdi zmdi-book"></i>Yoginth</a>
+<br>
+<i class="zmdi zmdi-collection-bookmark"></i> Page is not in a chapter
+</div>
+</div>
+</div>
+<hr>
+<div class="page  entity-list-item" data-entity-type="page" data-entity-id="1">
+<h4>
+<a href="https://docspen.ga/books/docspen/page/about" class="text-page entity-list-item-link"><i class="zmdi zmdi-file-text"></i><span class="entity-list-item-name">About</span></a>
+</h4>
+<div class="entity-item-snippet" style="font-size:14px;line-height:20px">
+<p class="text-muted">DocsPen is an online documentation or wiki system that provides a pleasant and simple out of the ...</p>
+</div>
+<div class="row meta text-muted text-small">
+<div class="col-md-6">
+<p class="text-muted small">
+Revision #2 <br> Created <span title="Sun, Dec 31, 2017 7:58 AM">5 days ago</span> by <a href="https://docspen.ga/@/3">Yoginth</a>
+<br>
+Updated <span title="Mon, Jan 1, 2018 12:14 PM">4 days ago</span> by <a href="https://docspen.ga/@/3">Yoginth</a>
+</p> </div>
+<div class="col-md-6">
+<a class="text-book" href="https://docspen.ga/books/docspen"><i class="zmdi zmdi-book"></i>DocsPen</a>
+<br>
+<i class="zmdi zmdi-collection-bookmark"></i> Page is not in a chapter
+</div>
+</div>
+</div>
+<hr>
+<div class="page  entity-list-item" data-entity-type="page" data-entity-id="5">
+<h4>
+<a href="https://docspen.ga/books/docspen/page/privacy" class="text-page entity-list-item-link"><i class="zmdi zmdi-file-text"></i><span class="entity-list-item-name">Privacy</span></a>
+</h4>
+<div class="entity-item-snippet" style="font-size:14px;line-height:20px">
+<p class="text-muted">What information do we collect?
+We collect information from you when you register on our site.
+...</p>
+</div>
+<div class="row meta text-muted text-small">
+<div class="col-md-6">
+<p class="text-muted small">
+Revision #1 <br> Created <span title="Sun, Dec 31, 2017 8:23 AM">5 days ago</span> by <a href="https://docspen.ga/@/3">Yoginth</a>
+<br>
+Updated <span title="Sun, Dec 31, 2017 8:30 AM">5 days ago</span> by <a href="https://docspen.ga/@/3">Yoginth</a>
+</p> </div>
+<div class="col-md-6">
+<a class="text-book" href="https://docspen.ga/books/docspen"><i class="zmdi zmdi-book"></i>DocsPen</a>
+<br>
+<i class="zmdi zmdi-collection-bookmark"></i> Page is not in a chapter
+</div>
+</div>
+</div>
+<hr>
+<div class="page  entity-list-item" data-entity-type="page" data-entity-id="4">
+<h4>
+<a href="https://docspen.ga/books/docspen/page/terms" class="text-page entity-list-item-link"><i class="zmdi zmdi-file-text"></i><span class="entity-list-item-name">Terms</span></a>
+</h4>
+<div class="entity-item-snippet" style="font-size:14px;line-height:20px">
+<p class="text-muted">By using the DocsPen (docspen.com) website ("Service"), you are agreeing to be bound by the follo...</p>
+</div>
+<div class="row meta text-muted text-small">
+<div class="col-md-6">
+<p class="text-muted small">
+Revision #1 <br> Created <span title="Sun, Dec 31, 2017 8:17 AM">5 days ago</span> by <a href="https://docspen.ga/@/3">Yoginth</a>
+<br>
+Updated <span title="Sun, Dec 31, 2017 8:23 AM">5 days ago</span> by <a href="https://docspen.ga/@/3">Yoginth</a>
+</p> </div>
+<div class="col-md-6">
+<a class="text-book" href="https://docspen.ga/books/docspen"><i class="zmdi zmdi-book"></i>DocsPen</a>
+<br>
+<i class="zmdi zmdi-collection-bookmark"></i> Page is not in a chapter
+</div>
+</div>
+</div> 
+<hr>
+<div class="page  entity-list-item" data-entity-type="page" data-entity-id="2">
+<h4>
+<a href="https://docspen.ga/books/blog/page/alpha" class="text-page entity-list-item-link"><i class="zmdi zmdi-file-text"></i><span class="entity-list-item-name">Alpha</span></a>
+</h4>
+<div class="entity-item-snippet" style="font-size:14px;line-height:20px">
+<p class="text-muted">I feel like I should research the history and meaning of using the term "Alpha" for software proj...</p>
+</div>
+<div class="row meta text-muted text-small">
+<div class="col-md-6">
+<p class="text-muted small">
+Revision #2 <br> Created <span title="Sun, Dec 31, 2017 8:10 AM">5 days ago</span> by <a href="https://docspen.ga/@/3">Yoginth</a>
+<br>
+Updated <span title="Sun, Dec 31, 2017 8:16 AM">5 days ago</span> by <a href="https://docspen.ga/@/3">Yoginth</a>
+</p> </div>
+<div class="col-md-6">
+<a class="text-book" href="https://docspen.ga/books/blog"><i class="zmdi zmdi-book"></i>Blog</a>
+<br>
+<i class="zmdi zmdi-collection-bookmark"></i> Page is not in a chapter
+</div>
+</div>
+</div>
+<hr>
+<div class="page  entity-list-item" data-entity-type="page" data-entity-id="3">
+<h4>
+<a href="https://docspen.ga/books/blog/page/beta" class="text-page entity-list-item-link"><i class="zmdi zmdi-file-text"></i><span class="entity-list-item-name">Beta</span></a>
+</h4>
+<div class="entity-item-snippet" style="font-size:14px;line-height:20px">
+<p class="text-muted">We made it to public beta! This was big for us. It's taken us 210 days (6 months, 26 days) and 49...</p>
+</div>
+<div class="row meta text-muted text-small">
+<div class="col-md-6">
+<p class="text-muted small">
+Revision #1 <br> Created <span title="Sun, Dec 31, 2017 8:13 AM">5 days ago</span> by <a href="https://docspen.ga/@/3">Yoginth</a>
+<br>
+Updated <span title="Sun, Dec 31, 2017 8:15 AM">5 days ago</span> by <a href="https://docspen.ga/@/3">Yoginth</a>
+</p> </div>
+<div class="col-md-6">
+<a class="text-book" href="https://docspen.ga/books/blog"><i class="zmdi zmdi-book"></i>Blog</a>
+<br>
+<i class="zmdi zmdi-collection-bookmark"></i> Page is not in a chapter
+</div>
+</div>
+</div>
+</div> <div class="body text-center">
+</div>
+</div>
+
+@stop
