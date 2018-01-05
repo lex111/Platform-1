36f15f2e2c55d6ef46ea83187c8e0e12c6ba2afa
Yoginth <yoginth@aol.com>
2018-01-05 13:37:35 +0000

    Added TOS

--- resources/views/others/terms.blade.php
+++ resources/views/others/terms.blade.php
@@ -9,18 +9,17 @@
 <div class="entity-list detailed" ng-non-bindable="">
 <div class="page  entity-list-item" data-entity-type="page" data-entity-id="7">
 <h4>
-<a href="https://docspen.ga/books/yoginth/page/googles-data-center" class="text-page entity-list-item-link"><i class="zmdi zmdi-file-text"></i><span class="entity-list-item-name">Google's Data Center</span></a>
+    <a href="https://docspen.ga/books/yoginth/page/googles-data-center" class="text-page entity-list-item-link"><i class="zmdi zmdi-file-text"></i><span class="entity-list-item-name">Google's Data Center</span></a>
 </h4>
     <div class="entity-item-snippet" style="font-size:14px;line-height:20px">
-    <p class="text-muted">Google's Data Center
-    First Video Embed!</p>
+        <p class="text-muted">Google's Data Center First Video Embed!</p>
     </div>
     <div class="row meta text-muted text-small">
         <div class="col-md-6">
             <p class="text-muted small">
             Revision #1 <br> Created <span title="Thu, Jan 4, 2018 4:17 AM">1 day ago</span> by <a href="https://docspen.ga/@/3">Yoginth</a>
             <br>
-            Updated <span title="Thu, Jan 4, 2018 4:30 AM">1 day ago</span> by <a href="https://docspen.ga/@/3">Yoginth</a>
+            Updated <span title="Thu, Jan 4, 2018 4:30 AM">1 day ago</span> by <a ></a>ef="https://docspen.ga/@/3">Yoginth</a>
             </p> </div>
             <div class="col-md-6">
             <a class="text-book" href="https://docspen.ga/books/yoginth"><i class="zmdi zmdi-book"></i>Yoginth</a>
@@ -29,8 +28,6 @@
             </div>
         </div>
     </div>
-    <hr>
-    <div class="page  entity-list-item" data-entity-type="page" data-entity-id="1">
 </div>
 </div>
 </div>
