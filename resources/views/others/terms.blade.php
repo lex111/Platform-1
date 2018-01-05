0f1031e4d7dc43ef06854341b632c06304c8247b
Yoginth <yoginth@aol.com>
2018-01-05 13:40:12 +0000

    Added TOS

--- resources/views/others/terms.blade.php
+++ resources/views/others/terms.blade.php
@@ -5,11 +5,11 @@
 <div class="container small">
     <p>&nbsp;</p>
     <div class="card">
-        <h3>Recently Updated Pages</h3>
+        <h3>Terms of Service</h3>
         <div class="entity-list detailed" ng-non-bindable="">
             <div class="page  entity-list-item" data-entity-type="page" data-entity-id="7">
                 <h4>
-                    <a href="https://docspen.ga/books/yoginth/page/googles-data-center" class="text-page entity-list-item-link"><i class="zmdi zmdi-file-text"></i><span class="entity-list-item-name">Google's Data Center</span></a>
+                    <a class="text-page entity-list-item-link"><i class="zmdi zmdi-file-text"></i><span class="entity-list-item-name">Terms of Service</span></a>
                 </h4>
                 <div class="entity-item-snippet" style="font-size:14px;line-height:20px">
                     <p class="text-muted">Google's Data Center First Video Embed!</p>
