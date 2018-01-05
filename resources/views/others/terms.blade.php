3ea6542a9391bbab0c2e626089c1c9a4d8c36f1b
Yoginth <yoginth@aol.com>
2018-01-05 13:39:00 +0000

    Added TOS

--- resources/views/others/terms.blade.php
+++ resources/views/others/terms.blade.php
@@ -3,23 +3,20 @@
 @section('content')
 
 <div class="container small">
-<p>&nbsp;</p>
-<div class="card">
-<h3>Recently Updated Pages</h3>
-<div class="entity-list detailed" ng-non-bindable="">
-<div class="page  entity-list-item" data-entity-type="page" data-entity-id="7">
-<h4>
-    <a href="https://docspen.ga/books/yoginth/page/googles-data-center" class="text-page entity-list-item-link"><i class="zmdi zmdi-file-text"></i><span class="entity-list-item-name">Google's Data Center</span></a>
-</h4>
-    <div class="entity-item-snippet" style="font-size:14px;line-height:20px">
-        <p class="text-muted">Google's Data Center First Video Embed!</p>
+    <p>&nbsp;</p>
+    <div class="card">
+        <h3>Recently Updated Pages</h3>
+        <div class="entity-list detailed" ng-non-bindable="">
+            <div class="page  entity-list-item" data-entity-type="page" data-entity-id="7">
+                <h4>
+                    <a href="https://docspen.ga/books/yoginth/page/googles-data-center" class="text-page entity-list-item-link"><i class="zmdi zmdi-file-text"></i><span class="entity-list-item-name">Google's Data Center</span></a>
+                </h4>
+                <div class="entity-item-snippet" style="font-size:14px;line-height:20px">
+                    <p class="text-muted">Google's Data Center First Video Embed!</p>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
-</div>
-</div>
-</div> <div class="body text-center">
-</div>
-</div>
 </div>
 
 @stop
