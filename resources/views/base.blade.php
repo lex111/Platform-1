cc3fa897672705aa6208645d0e3298505aef989d
Yoginth <yoginth@aol.com>
2018-01-06 16:47:48 +0000

    Added Common Header

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -3,27 +3,8 @@
 <head>
     <title>{{ isset($pageTitle) ? $pageTitle . ' | ' : '' }}{{ setting('app-name') }}</title>
     
-    <link rel="dns-prefetch" href="https://docspen.ga">
-    <link rel="dns-prefetch" href="https://unpkg.com">
-    <link rel="dns-prefetch" href="https://cdnjs.cloudflare.com">
-    <link rel="dns-prefetch" href="https://i1.wp.com">
+    @include('inc.header')
     
-    <meta name="viewport" content="width=device-width">
-    <meta name="token" content="{{ csrf_token() }}">
-    <meta name="base-url" content="{{ baseUrl('/') }}">
-    <meta name="theme-color" content="#026AA7">
-    <meta name="mobile-web-app-capable" content="yes">
-    <meta charset="utf-8">
-    <meta name="description" content="DocsPen - Online Documentation Platform. Read more, know more.">
-    <meta name="keywords" content="DocsPen, Docs, Documentation, Project Documentation, Wiki">
-
-    <link rel="icon" type="image/png" href="https://unpkg.com/docspen@7.0.0/imgs/favicon.png" />
-    <link rel="stylesheet" href="{{ hashed_asset('css/styles.css') }}">
-    <link rel="stylesheet" media="print" href="{{ hashed_asset('css/print-styles.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
-    <link rel="manifest" href="/manifest.json">
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
     <script src="{{ baseUrl('/translations') }}"></script>
     <script>
