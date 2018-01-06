86202558e2fb4894e95b684b2d54ce9df3663c52
Yoginth <yoginth@aol.com>
2018-01-06 11:55:10 +0000

    Added target=_blank for 3rd party

--- resources/views/partials/_header-dropdown.blade.php
+++ resources/views/partials/_header-dropdown.blade.php
@@ -13,15 +13,15 @@
         @if(signedInUser() && userCan('settings-manage'))
             <hr style="margin-bottom:10px">
             <a href="{{ baseUrl('/settings') }}" class="text-primary"><i class="zmdi zmdi-compass"></i>Admin</a>
-            <a href="{{ baseUrl('/git') }}" class="text-primary"><i class="zmdi zmdi-github"></i>GitHub</a>
-            <a href="{{ baseUrl('/trello') }}" class="text-primary"><i class="zmdi zmdi-view-carousel"></i>Trello</a>
+            <a href="{{ baseUrl('/git') }}" target="_blank" class="text-primary"><i class="zmdi zmdi-github"></i>GitHub</a>
+            <a href="{{ baseUrl('/trello') }}" target="_blank" class="text-primary"><i class="zmdi zmdi-view-carousel"></i>Trello</a>
         @endif
         <hr style="margin-bottom:10px">
         <li>
-            <a href="{{ baseUrl('/blog') }}" class="text-primary"><i class="zmdi zmdi-tumblr"></i>Blog</a>
+            <a href="{{ baseUrl('/blog') }}" target="_blank" class="text-primary"><i class="zmdi zmdi-tumblr"></i>Blog</a>
         </li>
         <li>
-            <a href="{{ baseUrl('/status') }}" class="text-primary"><i class="zmdi zmdi-check-all"></i>Status</a>
+            <a href="{{ baseUrl('/status') }}" target="_blank" class="text-primary"><i class="zmdi zmdi-check-all"></i>Status</a>
         </li>
         <li>
             <a href="{{ baseUrl('/logout') }}" class="text-neg"><i class="zmdi zmdi-run"></i>{{ trans('auth.logout') }}</a>
