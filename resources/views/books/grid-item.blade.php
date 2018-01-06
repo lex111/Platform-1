9d7662a693f314c6492a0e1db294d5208eca1576
Yoginth <yoginth@aol.com>
2018-01-06 05:49:11 +0000

    Updated Books Grid

--- resources/views/books/grid-item.blade.php
+++ resources/views/books/grid-item.blade.php
@@ -1,9 +1,9 @@
 <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 book-grid-item"  data-entity-type="book" data-entity-id="{{$book->id}}">
-    <div class="featured-image-container">
-        <a href="{{$book->getUrl()}}" title="{{$book->name}}">
+    <a href="{{$book->getUrl()}}" title="{{$book->name}}">
+        <div class="featured-image-container">
             <img width="1600" height="900" src="{{$book->getBookCover()}}?quality=100" alt="{{$book->name}}">
-        </a>
-    </div>
+        </div>
+    </a>
     <div class="book-grid-content">
         <h2><a href="{{$book->getUrl()}}" title="{{$book->name}}" > {{$book->getShortName(35)}} </a></h2>
         @if(isset($book->searchSnippet))
