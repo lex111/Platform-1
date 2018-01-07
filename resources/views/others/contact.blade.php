e2dde98aa8d6266b6f315b5371b4d8efe899227b
Yoginth <yoginth@aol.com>
2018-01-07 14:35:13 +0000

    Updated Contact Page

--- resources/views/others/contact.blade.php
+++ resources/views/others/contact.blade.php
@@ -4,7 +4,12 @@
    <div class="text-center">
         <div class="card auth-border center-box">
             <div class="body">
-                <form action="{{ baseUrl("/register") }}" method="POST">
+                <form accept-charset="UTF-8" action="https://usebasin.com/f/1d7f387e41bb" enctype="multipart/form-data" method="POST">
+    
+                    <div class="form-group">
+                        <label for="email">Name</label>
+                        <input class="input" type="text" name="name">
+                    </div>
 
                     <div class="form-group">
                         <label for="email">E-Mail</label>
@@ -12,18 +17,16 @@
                     </div>
 
                     <div class="form-group">
-                        <label for="email">{{ trans('auth.email') }}</label>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="password">{{ trans('auth.password') }}</label>
+                        <label for="password">Message</label>
                         <textarea class="textarea" placeholder="I'm a human. Please be nice." name="message" minlength="5" required autofocus></textarea>
                     </div>
 
                     <div class="from-group">
-                        <button class="button block pos"><i class="zmdi zmdi-plus"></i>{{ trans('auth.create_account') }}</button>
+                        <button class="button block pos" type="submit"><i class="zmdi zmdi-plus"></i>Submit</button>
                     </div>
+                    
                 </form>
+                
             </div>
         </div>
     </div>
