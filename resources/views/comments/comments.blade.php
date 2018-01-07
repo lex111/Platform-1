824e8b673610fc44327f67dbcbf8578c802b1cb9
Yoginth <yoginth@aol.com>
2018-01-07 14:32:50 +0530

    Update comments.blade.php

--- resources/views/comments/comments.blade.php
+++ resources/views/comments/comments.blade.php
@@ -1,5 +1,5 @@
 <div page-comments page-id="{{ $page->id }}" ng-non-bindable class="comments-list">
-  <h3 comments-title>{{ trans_choice('entities.comment_count', count($page->comments), ['count' => count($page->comments)]) }}</h3>
+  <h5 comments-title>{{ trans_choice('entities.comment_count', count($page->comments), ['count' => count($page->comments)]) }}</h5>
 
     <div class="comment-container" comment-container>
         @foreach($page->comments as $comment)
