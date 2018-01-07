1c9370866776ca6168194451e48cf3e28fefd688
Yoginth <yoginth@aol.com>
2018-01-07 14:57:35 +0000

    Added Success if Mail Sent

--- resources/views/others/success.blade.php
+++ resources/views/others/success.blade.php
@@ -8,9 +8,11 @@
 
     <div class="card" style="background-color:transparent;box-shadow:none">
         <div class="body">
-            <center><img src="https://unpkg.com/docspen@15.0.0/imgs/success.svg" style="width:100%;height:13em;pointer-events:none"></center>
-            <center><p><a href="{{ baseUrl('/') }}" class="button outline" style="margin-top:34px">{{ trans('errors.return_home') }}</a></p></center>
-            <h2>Message Received</h2>
+            <center>
+                <img src="https://unpkg.com/docspen@15.0.0/imgs/success.svg" style="width:100%;height:13em;pointer-events:none">
+                <h2>Message Received</h2>
+                <p><a href="{{ baseUrl('/') }}" class="button outline" style="margin-top:34px">{{ trans('errors.return_home') }}</a></p>
+            </center>
         </div>
     </div>
 
