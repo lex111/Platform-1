ea9713078821cb8a4670d0f69836f3e6b5ace25b
Yoginth <yoginth@aol.com>
2018-01-07 17:20:47 +0530

    Updated Login Page

--- resources/views/blank.blade.php
+++ resources/views/blank.blade.php
@@ -1,17 +1,15 @@
 <!DOCTYPE html>
-<html class="shaded" style="background:#f9f9f9">
+<html class="shaded" style="background-color:#f2f2f2;background-image: url(https://unpkg.com/docspen@14.0.0/imgs/pattern.svg)">
 <head>
-    <title>{{ setting('app-name') }}</title>
+    <title>{{ setting('app-name') }} - Auth</title>
     
     @include('inc.header')
-    
     @include('partials.custom-styles')
 
 </head>
 <body class="@yield('body-class')">
 
 @include('partials.notifications')
-
 
 <section class="container">
     @yield('content')
