3d21a2317c4a10a7eca9cb3778768d88fca34ab6
Yoginth <yoginth@aol.com>
2018-01-07 04:33:21 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/OthersController.php
+++ app/Http/Controllers/OthersController.php
@@ -14,31 +14,31 @@ class OthersController extends Controller
     {
         return view('others.terms');
     }
-    
+
     // ToDos on Trello Kanban
     public function trello()
     {
         return redirect('https://trello.com/b/nUennIKj/docspen');
     }
-    
+
     // Source code on GitHub
     public function git()
     {
         return redirect('https://github.com/DocsPen/Platform');
     }
-    
+
     // Blog on Tumblr
     public function blog()
     {
         return redirect('https://docspen.tumblr.com');
     }
-    
+
     // Status Page
     public function status()
     {
         return redirect('https://stats.uptimerobot.com/jZDKmIREm');
     }
-    
+
     // For Status used for checking Hearbeat of Server
     public function ping()
     {
