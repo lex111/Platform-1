93bb15916d14b09cb08658f0175a4e3de0c29e38
Yoginth <yoginth@aol.com>
2018-01-07 14:56:23 +0000

    Added Success if Mail Sent

--- resources/views/others/success.blade.php
+++ resources/views/others/success.blade.php
@@ -10,6 +10,7 @@
         <div class="body">
             <center><img src="https://unpkg.com/docspen@15.0.0/imgs/success.svg" style="width:100%;height:13em;pointer-events:none"></center>
             <center><p><a href="{{ baseUrl('/') }}" class="button outline" style="margin-top:34px">{{ trans('errors.return_home') }}</a></p></center>
+            <h2>Message Received</h2>
         </div>
     </div>
 
