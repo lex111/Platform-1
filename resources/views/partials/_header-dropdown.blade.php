08fb8324a3c5ce468436e8b6f3641eeb666baf0e
Yoginth <yoginth@aol.com>
2018-01-07 15:18:36 +0000

    Updated Contact Form

--- resources/views/partials/_header-dropdown.blade.php
+++ resources/views/partials/_header-dropdown.blade.php
@@ -27,6 +27,9 @@
             <a href="{{ baseUrl('/status') }}" target="_blank" class="text-primary"><i class="zmdi zmdi-check-all"></i>Status</a>
         </li>
         <li>
+            <a href="{{ baseUrl('/contact') }}" target="_blank" class="text-primary"><i class="zmdi zmdi-email"></i>Contact</a>
+        </li>
+        <li>
             <a href="{{ baseUrl('/logout') }}" class="text-neg"><i class="zmdi zmdi-run"></i>{{ trans('auth.logout') }}</a>
         </li>
     </ul>
