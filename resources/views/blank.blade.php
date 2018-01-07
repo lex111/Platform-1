264c435b991cea2fa0f0bb79833e0fb996eadd94
Yoginth <yoginth@aol.com>
2018-01-07 17:29:09 +0530

    Updated Styles

--- resources/views/blank.blade.php
+++ resources/views/blank.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html class="shaded" style="background-color:#f2f2f2;background-image: url(https://unpkg.com/docspen@14.0.0/imgs/pattern.svg)">
+<html class="auth-style">
 <head>
     <title>{{ setting('app-name') }} - Auth</title>
     
