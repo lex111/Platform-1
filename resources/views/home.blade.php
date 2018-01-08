579cd32ef1a92212f2c148bdaab06d760b7da279
Yoginth <yoginth@aol.com>
2018-01-08 21:54:37 +0530

    Changed Syntax for @include to .

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,13 +17,13 @@
                 @if(count($draftPages) > 0)
                     <div id="recent-drafts" class="card">
                         <h3><i class="zmdi zmdi-edit"></i> {{ trans('entities.my_recent_drafts') }}</h3>
-                        @include('partials/entity-list', ['entities' => $draftPages, 'style' => 'compact'])
+                        @include('partials.entity-list', ['entities' => $draftPages, 'style' => 'compact'])
                     </div>
                 @endif
 
                 <div class="card">
                     <h3><i class="zmdi zmdi-{{ $signedIn ? 'eye' : 'star-circle' }}" style="color:#e27b41"></i> {{ trans('entities.' . ($signedIn ? 'my_recently_viewed' : 'books_recent')) }}</h3>
-                    @include('partials/entity-list', [
+                    @include('partials.entity-list', [
                         'entities' => $recents,
                         'style' => 'compact',
                         'emptyText' => $signedIn ? trans('entities.no_pages_viewed') : trans('entities.books_empty')
@@ -46,7 +46,7 @@
                 @if(signedInUser())
                     <div class="card">
                         <h3><i class="zmdi zmdi-time"></i> {{ trans('entities.recent_activity') }}</h3>
-                        @include('partials/activity-list', ['activity' => $activity])
+                        @include('partials.activity-list', ['activity' => $activity])
                     </div>
                 @else
                     <div class="card">
