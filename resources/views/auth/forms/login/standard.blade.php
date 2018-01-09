4d4a9e337bcc424d72b79ec91f5e635f4698f8f6
Yoginth <yoginth@aol.com>
2018-01-09 12:36:56 +0000

    MC

--- resources/views/auth/forms/login/standard.blade.php
+++ resources/views/auth/forms/login/standard.blade.php
@@ -1,13 +1,4 @@
 <div class="form-group">
-<<<<<<< HEAD
-    <label for="email">{{ trans('auth.email') }}</label>
-    @include('form/text', ['name' => 'email', 'tabindex' => 1])
-</div>
-
-<div class="form-group">
-    <label for="password">{{ trans('auth.password') }}</label>
-    @include('form/password', ['name' => 'password', 'tabindex' => 2])
-=======
     <label for="email" required>{{ trans('auth.email') }}</label>
     @include('form.text', ['name' => 'email', 'tabindex' => 1])
 </div>
@@ -15,6 +6,5 @@
 <div class="form-group">
     <label for="password" required>{{ trans('auth.password') }}</label>
     @include('form.password', ['name' => 'password', 'tabindex' => 2])
->>>>>>> 0301ba6234aea7f405b61d35174b679172400e2d
     <span class="block small"><a href="{{ baseUrl('/password/email') }}">{{ trans('auth.forgot_password') }}</a></span>
 </div>
