9cffae4a8a20a4344cbe0fb1b059bf980ecdac85
Yoginth <yoginth@aol.com>
2018-01-12 12:33:36 +0000

    Updated Right Arrow to CDNJS

--- resources/views/chapters/_breadcrumbs.blade.php
+++ resources/views/chapters/_breadcrumbs.blade.php
@@ -1,7 +1,7 @@
 <div class="breadcrumbs">
     @if (userCan('view', $chapter->book))
     <a href="{{ $chapter->book->getUrl() }}" class="text-book text-button"><i class="zmdi zmdi-book"></i>{{ $chapter->book->getShortName() }}</a>
-    <img src="https://cdn.jsdelivr.net/npm/docspen@6.0.0/imgs/arrow-right.svg" style="height:10px">
+    <img src="https://cdnjs.cloudflare.com/ajax/libs/uswds/1.4.4/img/arrow-right.svg" style="height:10px">
     @endif
     <a href="{{ $chapter->getUrl() }}" class="text-chapter text-button"><i class="zmdi zmdi-collection-bookmark"></i>{{$chapter->getShortName()}}</a>
 </div>
