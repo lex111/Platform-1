2628263c84bfff88c0d75d34154ef7ece0b05740
Yoginth <yoginth@aol.com>
2018-01-12 21:29:11 +0530

    Update success.blade.php

--- resources/views/others/success.blade.php
+++ resources/views/others/success.blade.php
@@ -9,7 +9,7 @@
     <div class="card" style="background-color:transparent;box-shadow:none">
         <div class="body">
             <center>
-                <img src="hhttps://cdnjs.cloudflare.com/ajax/libs/twemoji/2.1.5/2/svg/2714.svg" style="width:100%;height:13em;pointer-events:none">
+                <img src="https://cdnjs.cloudflare.com/ajax/libs/twemoji/2.1.5/2/svg/2714.svg" style="width:100%;height:13em;pointer-events:none">
                 <h2>Message Received</h2>
                 <p><a href="{{ baseUrl('/') }}" class="button outline" style="margin-top:34px">{{ trans('errors.return_home') }}</a></p>
             </center>
