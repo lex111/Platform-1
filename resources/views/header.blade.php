b9731410aa3d255ed065f5bda12c183022dcbe66
Yoginth <yoginth@aol.com>
2018-01-12 12:16:26 +0000

    Removed Whitespace in header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,7 +1,7 @@
     <link rel="dns-prefetch" href="https://docspen.ga">
     <link rel="dns-prefetch" href="https://cdn.jsdelivr.net">
     <link rel="dns-prefetch" href="https://i0.wp.com">
-    
+
     <meta name="viewport" content="width=device-width">
     <meta name="token" content="{{ csrf_token() }}">
     <meta name="base-url" content="{{ baseUrl('/') }}">
@@ -10,7 +10,7 @@
     <meta charset="utf-8">
     <meta name="description" content="DocsPen - Online Documentation Platform. Read more, know more.">
     <meta name="keywords" content="DocsPen, Docs, Documentation, Project Documentation, Wiki">
-    
+
     <link rel="icon" href="/favicon.ico" />
     <link rel="stylesheet" href="{{ hashed_asset('css/styles.css') }}">
     <link rel="stylesheet" media="print" href="{{ hashed_asset('css/print-styles.css') }}">
