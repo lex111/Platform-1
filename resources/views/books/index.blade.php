fc415279a9974f3ec283d034db8444a1e91c0d68
Yoginth <yoginth@aol.com>
2018-01-15 11:35:07 +0000

    Added Topic

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -20,34 +20,7 @@
                         <i class="zmdi zmdi-caret-down"></i>
                     </span>
                     <ul>
-                        <li>
-                            <a href="{{ baseUrl("/@/{$currentUser->id}") }}" class="text-primary"><i class="zmdi zmdi-account"></i>{{ $currentUser->getShortName(9) }}</a>
-                        </li>
-                        <li>
-                            <a href="{{ baseUrl("/settings/users/{$currentUser->id}") }}" class="text-primary"><i class="zmdi zmdi-edit"></i>{{ trans('common.edit_profile') }}</a>
-                        </li>
-                        @if(signedInUser() && userCan('settings-manage'))
-                            <hr style="margin-bottom:10px">
-                            <a href="{{ baseUrl('/settings') }}" class="text-primary"><i class="zmdi zmdi-compass"></i>Admin</a>
-                            <a href="{{ baseUrl('/git') }}" target="_blank" class="text-primary"><i class="zmdi zmdi-github"></i>GitHub</a>
-                            <a href="{{ baseUrl('/trello') }}" target="_blank" class="text-primary"><i class="zmdi zmdi-view-carousel"></i>Trello</a>
-                        @endif
-                        <hr style="margin-bottom:10px">
-                        <li>
-                            <a href="{{ baseUrl('/blog') }}" target="_blank" class="text-primary"><i class="zmdi zmdi-tumblr"></i>Blog</a>
-                        </li>
-                        <li>
-                            <a href="{{ baseUrl('/terms') }}" class="text-primary"><i class="zmdi zmdi-assignment-check"></i>Terms</a>
-                        </li>
-                        <li>
-                            <a href="{{ baseUrl('/status') }}" target="_blank" class="text-primary"><i class="zmdi zmdi-check-all"></i>Status</a>
-                        </li>
-                        <li>
-                            <a href="{{ baseUrl('/contact') }}" class="text-primary"><i class="zmdi zmdi-email"></i>Contact</a>
-                        </li>
-                        <li>
-                            <a href="{{ baseUrl('/logout') }}" class="text-neg"><i class="zmdi zmdi-run"></i>{{ trans('auth.logout') }}</a>
-                        </li>
+                        <li><a href="{{ baseUrl('/search?term=%5Btechnology%5D') }}" class="text-primary">Technology</a></li>
                     </ul>
                 </div>
             </form>
