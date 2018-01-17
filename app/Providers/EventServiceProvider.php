2ec9a1ebf28ec153412a6e635c09295074065655
Yoginth <yoginth@aol.com>
2018-01-17 02:09:42 +0000

    Removed Slack and Azure

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,7 +3,6 @@
 namespace DocsPen\Providers;
 
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use SocialiteProviders\Manager\SocialiteWasCalled;
 
 class EventServiceProvider extends ServiceProvider
 {
