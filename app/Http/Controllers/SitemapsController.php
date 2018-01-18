1d23772227fef613a7af463588c8b3dbd3392993
Yoginth <yoginth@aol.com>
2018-01-18 15:37:11 +0000

    Updated Sitemap Controller

--- app/Http/Controllers/SitemapsController.php
+++ app/Http/Controllers/SitemapsController.php
@@ -11,38 +11,4 @@ class SitemapsController extends Controller
         return response()->view('sitemaps.index')
                     ->header('Content-Type', 'text/xml');
     }
-
-    public function books()
-    {
-        return response()->view('sitemaps.books')
-                    ->header('Content-Type', 'text/xml');
-    }
-
-    public function pages()
-    {
-        return response()->view('sitemaps.pages')
-                    ->header('Content-Type', 'text/xml');
-    }
-
-    public function chapters()
-    {
-        return response()->view('sitemaps.chapters')
-                    ->header('Content-Type', 'text/xml');
-    }
-
-    public function users()
-    {
-        $this->checkPermission('users-manage');
-        $listDetails = [
-            'order'  => $request->get('order', 'asc'),
-            'search' => $request->get('search', ''),
-            'sort'   => $request->get('sort', 'name'),
-        ];
-        $users = $this->userRepo->getAllUsersPaginatedAndSorted(20, $listDetails);
-        $this->setPageTitle(trans('settings.users'));
-        $users->appends($listDetails);
-
-        return response()->view('sitemaps.users', ['users' => $users])
-                    ->header('Content-Type', 'text/xml');
-    }
 }
