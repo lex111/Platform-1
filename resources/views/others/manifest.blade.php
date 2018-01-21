c49ff9ed55e7c0ec5708fdf944c85f9a9d8fbae8
Yoginth <yoginth@aol.com>
2018-01-21 14:51:09 +0000

    Added Manifest to Blade

---
+++ resources/views/others/manifest.blade.php
@@ -0,0 +1,15 @@
+{
+  "short_name": "DocsPen Lite",
+  "name": "DocsPen Lite",
+  "icons": [
+    {
+      "src": "https://i0.wp.com/unpkg.com/docspen@@17.0.0/imgs/icon.png?quality=100&w=192",
+      "type": "image/png",
+      "sizes": "192x192"
+    }
+  ],
+  "display": "standalone",
+  "theme_color": "#026AA7",
+  "background_color": "#026AA7",
+  "start_url": "/?launcher=true"
+}
