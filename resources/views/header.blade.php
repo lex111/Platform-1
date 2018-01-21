4e128df195eb30e532e6bcc4b50994c6282046a6
Yoginth <yoginth@aol.com>
2018-01-21 09:30:50 +0000

    Updated Header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -5,14 +5,17 @@
     <link rel="assets" href="https://cdn.jsdelivr.net">
     <link rel="assets" href="https://cdnjs.cloudflare.com">
     <link rel="canonical" href="{{ baseUrl('/') }}">
+    <link rel="search" type="application/opensearchdescription+xml" href="/opensearch.xml" title="DocsPen">
+    
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
     <meta name="token" content="{{ csrf_token() }}">
     <meta name="base-url" content="{{ baseUrl('/') }}">
     <meta name="theme-color" content="#026AA7">
     <meta name="mobile-web-app-capable" content="yes">
-    <meta charset="utf-8">
     <meta name="description" content="DocsPen - Online Documentation Platform. Read more, know more.">
     <meta name="keywords" content="DocsPen, Docs, Documentation, Project Documentation, Wiki">
+    
     <link rel="icon" type="image/x-icon" href="/favicon.ico" />
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" sizes="16x16 24x24 32x32 64x64"/>
     <link rel="stylesheet" href="{{ hashed_asset('css/styles.css') }}">
@@ -20,7 +23,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
     <link rel="manifest" href="/manifest.json">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-    <link rel="search" type="application/opensearchdescription+xml" href="/opensearch.xml" title="DocsPen">
 
     <meta property="fb:app_id" content="">
     <meta property="og:url" content="https://docspen.ga">
