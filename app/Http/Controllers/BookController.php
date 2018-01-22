36de8d18cb7ce4a0c15caef418589f3d038bef54
Yoginth <yoginth@aol.com>
2018-01-22 12:55:31 +0000

    Updated Book String Size

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -79,7 +79,7 @@ class BookController extends Controller
     {
         $this->checkPermission('book-create-all');
         $this->validate($request, [
-            'name'        => 'required|string|max:20',
+            'name'        => 'required|string|max:30',
             'description' => 'string|max:1000',
         ]);
         $book = $this->entityRepo->createFromInput('book', $request->all());
