0b1cb90e589dd5e7f884fe0463484b729a9db6ed
Yoginth <yoginth@aol.com>
2018-01-23 12:25:03 +0000

    Added About Page

---
+++ resources/views/others/about.blade.php
@@ -0,0 +1,90 @@
+@extends('simple-layout')
+
+@section('content')
+
+<div class="container small">
+    <p>&nbsp;</p>
+    <div class="card">
+        <div class="entity-list detailed" ng-non-bindable="">
+            <div class="page  entity-list-item" data-entity-type="page" data-entity-id="7">
+                <h4>
+                    <i class="zmdi zmdi-assignment-check"></i><span class="entity-list-item-name">About</span>
+                </h4>
+                <div class="entity-item-snippet" style="font-size:14px;line-height:20px">
+                    <p>
+                        <br>
+                        Documenting projects is hard, hosting them shouldn’t be. DocsPen was created to make hosting documentation simple.
+                        <br>
+                        DocsPen is a hub to publish the documentation for projects through online, its right choice for Professors & Teachers
+                        for publishing the content, centralized platform for wikis, which provides a pleasant and simple out of the box experience.
+                        <br>
+                        New users to an instance should find the experience intuitive and only basic word-processing skills should be required to
+                        get involved in creating content on DocsPen.
+                        <br>
+                        The platform should provide advanced power features to those that desire it but they should not interfere with the core 
+                        simple user experience.
+                        <br>
+                        First, DocsPen was started as a weekend project for self-hosting our internal documents like API Documentations, Product 
+                        details etc. The project was taken out of the box as Open Source and we got some contributor across the globe and they 
+                        proposed us some ideas to take your project and ship it as a product to the globe.
+                        <br>
+                        So finally we started working on the conversion of (Project to a Product) transition and with hard working after 210 days 
+                        (6 months, 26 days) and 490,989 lines of code where done still that we are in Alpha Phase and we transitioned to Public 
+                        Beta Phase on January 1, 2018.
+                    </p>
+                    
+                    <p>
+                        DocsPen reserves the right to update and change the Terms of Service from time to time without notice. Any new features that 
+                        augment or enhance the current Service, including the release of new tools and resources, shall be subject to the Terms of 
+                        Service. Continued use of the Service after any such changes shall constitute your consent to such changes. You can review 
+                        the most current version of the Terms of Service at any time at  https://docspen.com/books/docspen/page/terms
+                        
+                        <br>
+                        
+                        Violation of any of the terms below will result in the termination of your Account. While DocsPen prohibits such conduct and 
+                        Content on the Service, you understand and agree that DocsPen cannot be responsible for the Content posted on the Service and
+                        you nonetheless may be exposed to such materials.You agree to use the Service at your own risk.
+                    </p>
+                    
+                    <h4>Account Terms</h4>
+                    <p>
+                        <ol>
+                            <li>You must be a human, bots are not allowed</li>
+                            <li>You must be 10 years or older to use this Service.</li>
+                            <li>You must provide a valid email address (as email).
+                            <li>You are responsible for maintaining the security of your account and password.</li>
+                            <li>You are responsible for all Content posted and activity that occurs under your account (even when Content is posted by others who have accounts under your account).6. One person may not maintain more than one free account.</li>
+                            <li>You may not use the Service for any illegal or unauthorized purpose. You must not, in the use of the Service, violate any laws in your jurisdiction</li>
+                            <li>Your use of the Service is at your sole risk</li>
+                            <li>You must not modify, adapt or hack the Service</li>
+                        </ol>
+                    </p>
+                    
+                    <h4>Copyright and Content Ownership</h4>
+                    <p>
+                        <ol>
+                            <li>The content (Images, Documents) is copyright ©2017 DocsPen, You may not duplicate, copy, or reuse any portion of the HTML/CSS, Javascript, or visual design elements or concepts without express written permission from DocsPen.</li>
+                            <li>Your profile and materials uploaded remain yours.</li>
+                            <li>Files edited and linked to your account remain yours</li>
+                            <li>DocsPen have the right (but not the obligation) in their sole discretion to refuse or remove any Content that is available via the Service.</li>
+                        </ol>
+                    </p>
+                                        
+                    <h4>Cancellation and Termination</h4>
+                    <p>
+                        <ol>
+                           <li>You have the right to cancel your account</li>
+                           <li>You are solely responsible for properly canceling your account.</li>
+                           <li>All of your Content will be immediately deleted from the Service upon cancellation. This information can not be recovered once your account is canceled.</li>
+                        </ol>
+                    </p>
+                    <p class="text-muted small">
+                        Revision #1 <br> Created <span title="Wed, Jan 5, 2018 4:17 AM">Wed, Jan 5, 2018 4:17 AM</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@stop
