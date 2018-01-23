acacddb4ddabdd3206c71d3a13c1cd9e7cd8d9a4
Yoginth <yoginth@aol.com>
2018-01-23 17:00:26 +0000

    Added green-inline-bg

--- resources/views/partials/custom-styles.blade.php
+++ resources/views/partials/custom-styles.blade.php
@@ -3,7 +3,7 @@
     .faded-small, .primary-background-light { background-color: {{ setting('app-color-light') }} }
     .button-base, .button, input[type="button"], input[type="submit"] { background-color: {{ setting('app-color') }}; border-color: {{ setting('app-color') }} }
     .button-base:hover, .button:hover, input[type="button"]:hover, input[type="submit"]:hover, .button:focus { background-color: {{ setting('app-color') }} }
-    .text-primary, p.primary, p .primary, span.primary:hover, .text-primary:hover, a, a:hover, a:focus, .text-button, .text-button:hover, .text-button:focus { color: {{ setting('app-color') }} }
+    .text-primary, p.primary, p .primary, span.primary:hover, .text-primary:hover, a, a:hover, a:focus, .text-button, .text-button:hover, .text-button:focus { color: #00acac }
 </style>
 
 @if(setting('app-custom-head') && \Route::currentRouteName() !== 'settings')
