ddd549b354b8d62ff59ebd42e8536c9a2d05751c
Yoginth <yoginth@aol.com>
2018-01-23 11:54:49 +0000

    Added Microsoft.URL.Control

--- resources/views/others/robots.blade.php
+++ resources/views/others/robots.blade.php
@@ -889,6 +889,9 @@ Disallow: /
 User-agent: linko
 Disallow: /
 
+User-agent: Microsoft.URL.Control
+Disallow: /
+
 #
 # Sorry, wget in its recursive mode is a frequent problem.
 # Please read the man page and use it properly; there is a
