d0b82000ae43dd0d8bb7fda4edddef0c313c0866
Yoginth <yoginth@aol.com>
2018-01-23 11:55:38 +0000

    Added libwww

--- resources/views/others/robots.blade.php
+++ resources/views/others/robots.blade.php
@@ -898,6 +898,9 @@ Disallow: /
 User-agent: larbin
 Disallow: /
 
+User-agent: libwww
+Disallow: /
+
 #
 # Sorry, wget in its recursive mode is a frequent problem.
 # Please read the man page and use it properly; there is a
