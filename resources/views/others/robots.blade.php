6cd56764ac7636dc3cdc7db0f0636947067a0bb4
Yoginth <yoginth@aol.com>
2018-01-23 11:55:10 +0000

    Added Xenu

--- resources/views/others/robots.blade.php
+++ resources/views/others/robots.blade.php
@@ -892,6 +892,9 @@ Disallow: /
 User-agent: Microsoft.URL.Control
 Disallow: /
 
+User-agent: 
+Disallow: /
+
 #
 # Sorry, wget in its recursive mode is a frequent problem.
 # Please read the man page and use it properly; there is a
