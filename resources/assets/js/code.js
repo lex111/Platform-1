6367e1bf6c71c9385820ea678d71e56feccb24b9
Yoginth <yoginth@aol.com>
2018-01-23 20:06:55 +0530

    Prettified JS Code

--- resources/assets/js/code.js
+++ resources/assets/js/code.js
@@ -1,66 +1,68 @@
-require('codemirror/mode/css/css');
-require('codemirror/mode/clike/clike');
-require('codemirror/mode/diff/diff');
-require('codemirror/mode/go/go');
-require('codemirror/mode/htmlmixed/htmlmixed');
-require('codemirror/mode/javascript/javascript');
-require('codemirror/mode/markdown/markdown');
-require('codemirror/mode/nginx/nginx');
-require('codemirror/mode/php/php');
-require('codemirror/mode/powershell/powershell');
-require('codemirror/mode/python/python');
-require('codemirror/mode/ruby/ruby');
-require('codemirror/mode/shell/shell');
-require('codemirror/mode/sql/sql');
-require('codemirror/mode/toml/toml');
-require('codemirror/mode/xml/xml');
-require('codemirror/mode/yaml/yaml');
+require("codemirror/mode/css/css");
+require("codemirror/mode/clike/clike");
+require("codemirror/mode/diff/diff");
+require("codemirror/mode/go/go");
+require("codemirror/mode/htmlmixed/htmlmixed");
+require("codemirror/mode/javascript/javascript");
+require("codemirror/mode/markdown/markdown");
+require("codemirror/mode/nginx/nginx");
+require("codemirror/mode/php/php");
+require("codemirror/mode/powershell/powershell");
+require("codemirror/mode/python/python");
+require("codemirror/mode/ruby/ruby");
+require("codemirror/mode/shell/shell");
+require("codemirror/mode/sql/sql");
+require("codemirror/mode/toml/toml");
+require("codemirror/mode/xml/xml");
+require("codemirror/mode/yaml/yaml");
 
-const CodeMirror = require('codemirror');
+const CodeMirror = require("codemirror");
 
 const modeMap = {
-    css: 'css',
-    c: 'clike',
-    java: 'clike',
-    scala: 'clike',
-    kotlin: 'clike',
-    'c++': 'clike',
-    'c#': 'clike',
-    csharp: 'clike',
-    diff: 'diff',
-    go: 'go',
-    html: 'htmlmixed',
-    javascript: 'javascript',
-    json: {name: 'javascript', json: true},
-    js: 'javascript',
-    php: 'php',
-    md: 'markdown',
-    mdown: 'markdown',
-    markdown: 'markdown',
-    nginx: 'nginx',
-    powershell: 'powershell',
-    py: 'python',
-    python: 'python',
-    ruby: 'ruby',
-    rb: 'ruby',
-    shell: 'shell',
-    sh: 'shell',
-    bash: 'shell',
-    toml: 'toml',
-    sql: 'sql',
-    xml: 'xml',
-    yaml: 'yaml',
-    yml: 'yaml',
+  css: "css",
+  c: "clike",
+  java: "clike",
+  scala: "clike",
+  kotlin: "clike",
+  "c++": "clike",
+  "c#": "clike",
+  csharp: "clike",
+  diff: "diff",
+  go: "go",
+  html: "htmlmixed",
+  javascript: "javascript",
+  json: { name: "javascript", json: true },
+  js: "javascript",
+  php: "php",
+  md: "markdown",
+  mdown: "markdown",
+  markdown: "markdown",
+  nginx: "nginx",
+  powershell: "powershell",
+  py: "python",
+  python: "python",
+  ruby: "ruby",
+  rb: "ruby",
+  shell: "shell",
+  sh: "shell",
+  bash: "shell",
+  toml: "toml",
+  sql: "sql",
+  xml: "xml",
+  yaml: "yaml",
+  yml: "yaml"
 };
 
 /**
  * Highlight pre elements on a page
  */
 function highlight() {
-    let codeBlocks = document.querySelectorAll('.page-content pre, .comment-box .content pre');
-    for (let i = 0; i < codeBlocks.length; i++) {
-        highlightElem(codeBlocks[i]);
-    }
+  let codeBlocks = document.querySelectorAll(
+    ".page-content pre, .comment-box .content pre"
+  );
+  for (let i = 0; i < codeBlocks.length; i++) {
+    highlightElem(codeBlocks[i]);
+  }
 }
 
 /**
@@ -68,24 +70,27 @@ function highlight() {
  * @param {HTMLElement} elem
  */
 function highlightElem(elem) {
-    let innerCodeElem = elem.querySelector('code[class^=language-]');
-    let mode = '';
-    if (innerCodeElem !== null) {
-        let langName = innerCodeElem.className.replace('language-', '');
-        mode = getMode(langName);
-    }
-    elem.innerHTML = elem.innerHTML.replace(/<br\s*[\/]?>/gi ,'\n');
-    let content = elem.textContent.trim();
+  let innerCodeElem = elem.querySelector("code[class^=language-]");
+  let mode = "";
+  if (innerCodeElem !== null) {
+    let langName = innerCodeElem.className.replace("language-", "");
+    mode = getMode(langName);
+  }
+  elem.innerHTML = elem.innerHTML.replace(/<br\s*[\/]?>/gi, "\n");
+  let content = elem.textContent.trim();
 
-    CodeMirror(function(elt) {
-        elem.parentNode.replaceChild(elt, elem);
-    }, {
-        value: content,
-        mode:  mode,
-        lineNumbers: true,
-        theme: getTheme(),
-        readOnly: true
-    });
+  CodeMirror(
+    function(elt) {
+      elem.parentNode.replaceChild(elt, elem);
+    },
+    {
+      value: content,
+      mode: mode,
+      lineNumbers: true,
+      theme: getTheme(),
+      readOnly: true
+    }
+  );
 }
 
 /**
@@ -94,8 +99,11 @@ function highlightElem(elem) {
  * @returns {string}
  */
 function getMode(suggestion) {
-    suggestion = suggestion.trim().replace(/^\./g, '').toLowerCase();
-    return (typeof modeMap[suggestion] !== 'undefined') ? modeMap[suggestion] : '';
+  suggestion = suggestion
+    .trim()
+    .replace(/^\./g, "")
+    .toLowerCase();
+  return typeof modeMap[suggestion] !== "undefined" ? modeMap[suggestion] : "";
 }
 
 /**
@@ -103,7 +111,7 @@ function getMode(suggestion) {
  * @returns {*|string}
  */
 function getTheme() {
-    return window.codeTheme || 'base16-light';
+  return window.codeTheme || "base16-light";
 }
 
 /**
@@ -113,41 +121,44 @@ function getTheme() {
  * @returns {{wrap: Element, editor: *}}
  */
 function wysiwygView(elem) {
-    let doc = elem.ownerDocument;
-    let codeElem = elem.querySelector('code');
+  let doc = elem.ownerDocument;
+  let codeElem = elem.querySelector("code");
 
-    let lang = (elem.className || '').replace('language-', '');
-    if (lang === '' && codeElem) {
-        lang = (codeElem.className || '').replace('language-', '')
+  let lang = (elem.className || "").replace("language-", "");
+  if (lang === "" && codeElem) {
+    lang = (codeElem.className || "").replace("language-", "");
+  }
+
+  elem.innerHTML = elem.innerHTML.replace(/<br\s*[\/]?>/gi, "\n");
+  let content = elem.textContent;
+  let newWrap = doc.createElement("div");
+  let newTextArea = doc.createElement("textarea");
+
+  newWrap.className = "CodeMirrorContainer";
+  newWrap.setAttribute("data-lang", lang);
+  newTextArea.style.display = "none";
+  elem.parentNode.replaceChild(newWrap, elem);
+
+  newWrap.appendChild(newTextArea);
+  newWrap.contentEditable = false;
+  newTextArea.textContent = content;
+
+  let cm = CodeMirror(
+    function(elt) {
+      newWrap.appendChild(elt);
+    },
+    {
+      value: content,
+      mode: getMode(lang),
+      lineNumbers: true,
+      theme: getTheme(),
+      readOnly: true
     }
-
-    elem.innerHTML = elem.innerHTML.replace(/<br\s*[\/]?>/gi ,'\n');
-    let content = elem.textContent;
-    let newWrap = doc.createElement('div');
-    let newTextArea = doc.createElement('textarea');
-
-    newWrap.className = 'CodeMirrorContainer';
-    newWrap.setAttribute('data-lang', lang);
-    newTextArea.style.display = 'none';
-    elem.parentNode.replaceChild(newWrap, elem);
-
-    newWrap.appendChild(newTextArea);
-    newWrap.contentEditable = false;
-    newTextArea.textContent = content;
-
-    let cm = CodeMirror(function(elt) {
-        newWrap.appendChild(elt);
-    }, {
-        value: content,
-        mode:  getMode(lang),
-        lineNumbers: true,
-        theme: getTheme(),
-        readOnly: true
-    });
-    setTimeout(() => {
-        cm.refresh();
-    }, 300);
-    return {wrap: newWrap, editor: cm};
+  );
+  setTimeout(() => {
+    cm.refresh();
+  }, 300);
+  return { wrap: newWrap, editor: cm };
 }
 
 /**
@@ -157,18 +168,21 @@ function wysiwygView(elem) {
  * @returns {*}
  */
 function popupEditor(elem, modeSuggestion) {
-    let content = elem.textContent;
+  let content = elem.textContent;
 
-    return CodeMirror(function(elt) {
-        elem.parentNode.insertBefore(elt, elem);
-        elem.style.display = 'none';
-    }, {
-        value: content,
-        mode:  getMode(modeSuggestion),
-        lineNumbers: true,
-        theme: getTheme(),
-        lineWrapping: true
-    });
+  return CodeMirror(
+    function(elt) {
+      elem.parentNode.insertBefore(elt, elem);
+      elem.style.display = "none";
+    },
+    {
+      value: content,
+      mode: getMode(modeSuggestion),
+      lineNumbers: true,
+      theme: getTheme(),
+      lineWrapping: true
+    }
+  );
 }
 
 /**
@@ -177,7 +191,7 @@ function popupEditor(elem, modeSuggestion) {
  * @param modeSuggestion
  */
 function setMode(cmInstance, modeSuggestion) {
-      cmInstance.setOption('mode', getMode(modeSuggestion));
+  cmInstance.setOption("mode", getMode(modeSuggestion));
 }
 
 /**
@@ -186,10 +200,10 @@ function setMode(cmInstance, modeSuggestion) {
  * @param codeContent
  */
 function setContent(cmInstance, codeContent) {
-    cmInstance.setValue(codeContent);
-    setTimeout(() => {
-        cmInstance.refresh();
-    }, 10);
+  cmInstance.setValue(codeContent);
+  setTimeout(() => {
+    cmInstance.refresh();
+  }, 10);
 }
 
 /**
@@ -198,18 +212,21 @@ function setContent(cmInstance, codeContent) {
  * @returns {*}
  */
 function markdownEditor(elem) {
-    let content = elem.textContent;
+  let content = elem.textContent;
 
-    return CodeMirror(function (elt) {
-        elem.parentNode.insertBefore(elt, elem);
-        elem.style.display = 'none';
-    }, {
-        value: content,
-        mode: "markdown",
-        lineNumbers: true,
-        theme: getTheme(),
-        lineWrapping: true
-    });
+  return CodeMirror(
+    function(elt) {
+      elem.parentNode.insertBefore(elt, elem);
+      elem.style.display = "none";
+    },
+    {
+      value: content,
+      mode: "markdown",
+      lineNumbers: true,
+      theme: getTheme(),
+      lineWrapping: true
+    }
+  );
 }
 
 /**
@@ -217,17 +234,17 @@ function markdownEditor(elem) {
  * @returns {string}
  */
 function getMetaKey() {
-    let mac = CodeMirror.keyMap["default"] == CodeMirror.keyMap.macDefault;
-    return mac ? "Cmd" : "Ctrl";
+  let mac = CodeMirror.keyMap["default"] == CodeMirror.keyMap.macDefault;
+  return mac ? "Cmd" : "Ctrl";
 }
 
 module.exports = {
-    highlight: highlight,
-    highlightElem: highlightElem,
-    wysiwygView: wysiwygView,
-    popupEditor: popupEditor,
-    setMode: setMode,
-    setContent: setContent,
-    markdownEditor: markdownEditor,
-    getMetaKey: getMetaKey,
+  highlight: highlight,
+  highlightElem: highlightElem,
+  wysiwygView: wysiwygView,
+  popupEditor: popupEditor,
+  setMode: setMode,
+  setContent: setContent,
+  markdownEditor: markdownEditor,
+  getMetaKey: getMetaKey
 };
--- resources/assets/js/global.js
+++ resources/assets/js/global.js
@@ -1,38 +1,42 @@
 "use strict";
 require("babel-polyfill");
-require('./dom-polyfills');
+require("./dom-polyfills");
 
 // Url retrieval function
 window.baseUrl = function(path) {
-    let basePath = document.querySelector('meta[name="base-url"]').getAttribute('content');
-    if (basePath[basePath.length-1] === '/') basePath = basePath.slice(0, basePath.length-1);
-    if (path[0] === '/') path = path.slice(1);
-    return basePath + '/' + path;
+  let basePath = document
+    .querySelector('meta[name="base-url"]')
+    .getAttribute("content");
+  if (basePath[basePath.length - 1] === "/")
+    basePath = basePath.slice(0, basePath.length - 1);
+  if (path[0] === "/") path = path.slice(1);
+  return basePath + "/" + path;
 };
 
 // Global Event System
 class EventManager {
-    constructor() {
-        this.listeners = {};
-        this.stack = [];
-    }
+  constructor() {
+    this.listeners = {};
+    this.stack = [];
+  }
 
-    emit(eventName, eventData) {
-        this.stack.push({name: eventName, data: eventData});
-        if (typeof this.listeners[eventName] === 'undefined') return this;
-        let eventsToStart = this.listeners[eventName];
-        for (let i = 0; i < eventsToStart.length; i++) {
-            let event = eventsToStart[i];
-            event(eventData);
-        }
-        return this;
+  emit(eventName, eventData) {
+    this.stack.push({ name: eventName, data: eventData });
+    if (typeof this.listeners[eventName] === "undefined") return this;
+    let eventsToStart = this.listeners[eventName];
+    for (let i = 0; i < eventsToStart.length; i++) {
+      let event = eventsToStart[i];
+      event(eventData);
     }
+    return this;
+  }
 
-    listen(eventName, callback) {
-        if (typeof this.listeners[eventName] === 'undefined') this.listeners[eventName] = [];
-        this.listeners[eventName].push(callback);
-        return this;
-    }
+  listen(eventName, callback) {
+    if (typeof this.listeners[eventName] === "undefined")
+      this.listeners[eventName] = [];
+    this.listeners[eventName].push(callback);
+    return this;
+  }
 }
 
 window.$events = new EventManager();
@@ -41,18 +45,29 @@ const Vue = require("vue");
 const axios = require("axios");
 
 let axiosInstance = axios.create({
-    headers: {
-        'X-CSRF-TOKEN': document.querySelector('meta[name=token]').getAttribute('content'),
-        'baseURL': window.baseUrl('')
-    }
+  headers: {
+    "X-CSRF-TOKEN": document
+      .querySelector("meta[name=token]")
+      .getAttribute("content"),
+    baseURL: window.baseUrl("")
+  }
 });
-axiosInstance.interceptors.request.use(resp => {
+axiosInstance.interceptors.request.use(
+  resp => {
     return resp;
-}, err => {
-    if (typeof err.response === "undefined" || typeof err.response.data === "undefined") return Promise.reject(err);
-    if (typeof err.response.data.error !== "undefined") window.$events.emit('error', err.response.data.error);
-    if (typeof err.response.data.message !== "undefined") window.$events.emit('error', err.response.data.message);
-});
+  },
+  err => {
+    if (
+      typeof err.response === "undefined" ||
+      typeof err.response.data === "undefined"
+    )
+      return Promise.reject(err);
+    if (typeof err.response.data.error !== "undefined")
+      window.$events.emit("error", err.response.data.error);
+    if (typeof err.response.data.message !== "undefined")
+      window.$events.emit("error", err.response.data.message);
+  }
+);
 window.$http = axiosInstance;
 
 Vue.prototype.$http = axiosInstance;
@@ -65,10 +80,8 @@ let translator = new Translations(window.translations);
 window.trans = translator.get.bind(translator);
 window.trans_choice = translator.getPlural.bind(translator);
 
-
 require("./vues/vues");
 require("./components");
-
 
 //Global jQuery Config & Extensions
 
@@ -77,12 +90,18 @@ require("./components");
  * @param {HTMLElement} element
  */
 window.scrollToElement = function(element) {
-    if (!element) return;
-    let offset = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
-    let top = element.getBoundingClientRect().top + offset;
-    $('html, body').animate({
-        scrollTop: top - 60 // Adjust to change final scroll position top margin
-    }, 300);
+  if (!element) return;
+  let offset =
+    window.pageYOffset ||
+    document.documentElement.scrollTop ||
+    document.body.scrollTop;
+  let top = element.getBoundingClientRect().top + offset;
+  $("html, body").animate(
+    {
+      scrollTop: top - 60 // Adjust to change final scroll position top margin
+    },
+    300
+  );
 };
 
 /**
@@ -90,60 +109,82 @@ window.scrollToElement = function(element) {
  * @param {HTMLElement} element
  */
 window.scrollAndHighlight = function(element) {
-    if (!element) return;
-    window.scrollToElement(element);
-    let color = document.getElementById('custom-styles').getAttribute('data-color-light');
-    let initColor = window.getComputedStyle(element).getPropertyValue('background-color');
-    element.style.backgroundColor = color;
-    setTimeout(() => {
-        element.classList.add('selectFade');
-        element.style.backgroundColor = initColor;
-    }, 10);
-    setTimeout(() => {
-        element.classList.remove('selectFade');
-        element.style.backgroundColor = '';
-    }, 3000);
+  if (!element) return;
+  window.scrollToElement(element);
+  let color = document
+    .getElementById("custom-styles")
+    .getAttribute("data-color-light");
+  let initColor = window
+    .getComputedStyle(element)
+    .getPropertyValue("background-color");
+  element.style.backgroundColor = color;
+  setTimeout(() => {
+    element.classList.add("selectFade");
+    element.style.backgroundColor = initColor;
+  }, 10);
+  setTimeout(() => {
+    element.classList.remove("selectFade");
+    element.style.backgroundColor = "";
+  }, 3000);
 };
 
 // Smooth scrolling
-jQuery.fn.smoothScrollTo = function () {
-    if (this.length === 0) return;
-    window.scrollToElement(this[0]);
-    return this;
+jQuery.fn.smoothScrollTo = function() {
+  if (this.length === 0) return;
+  window.scrollToElement(this[0]);
+  return this;
 };
 
 // Making contains text expression not worry about casing
-jQuery.expr[":"].contains = $.expr.createPseudo(function (arg) {
-    return function (elem) {
-        return $(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
-    };
+jQuery.expr[":"].contains = $.expr.createPseudo(function(arg) {
+  return function(elem) {
+    return (
+      $(elem)
+        .text()
+        .toUpperCase()
+        .indexOf(arg.toUpperCase()) >= 0
+    );
+  };
 });
 
 // Detect IE for css
-if(navigator.userAgent.indexOf('MSIE')!==-1
-    || navigator.appVersion.indexOf('Trident/') > 0
-    || navigator.userAgent.indexOf('Safari') !== -1){
-    document.body.classList.add('flexbox-support');
+if (
+  navigator.userAgent.indexOf("MSIE") !== -1 ||
+  navigator.appVersion.indexOf("Trident/") > 0 ||
+  navigator.userAgent.indexOf("Safari") !== -1
+) {
+  document.body.classList.add("flexbox-support");
 }
 
-if ('serviceWorker' in navigator && navigator.userAgent.indexOf("Mobile") === -1) {
-	navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-		console.log('ServiceWorker registration successful with scope: ', registration.scope);
-		console.log('DocsPen ♥\'s web');
-	})
-	.catch(function(err) {
-		console.error('ServiceWorker registration failed: ', err);
-	});
-} else if('serviceWorker' in navigator && navigator.userAgent.indexOf("Mobile") > -1){
-    navigator.serviceWorker.getRegistration().then(function(registration) {
-        var serviceWorkerUnregistered=false;
-        if(registration) {
-            registration.unregister();
-            serviceWorkerUnregistered=true;
-        }
-        serviceWorkerUnregistered && window.location.reload();
+if (
+  "serviceWorker" in navigator &&
+  navigator.userAgent.indexOf("Mobile") === -1
+) {
+  navigator.serviceWorker
+    .register("/service-worker.js")
+    .then(function(registration) {
+      console.log(
+        "ServiceWorker registration successful with scope: ",
+        registration.scope
+      );
+      console.log("DocsPen ♥'s web");
+    })
+    .catch(function(err) {
+      console.error("ServiceWorker registration failed: ", err);
     });
+} else if (
+  "serviceWorker" in navigator &&
+  navigator.userAgent.indexOf("Mobile") > -1
+) {
+  navigator.serviceWorker.getRegistration().then(function(registration) {
+    var serviceWorkerUnregistered = false;
+    if (registration) {
+      registration.unregister();
+      serviceWorkerUnregistered = true;
+    }
+    serviceWorkerUnregistered && window.location.reload();
+  });
 }
-        
+
 // Page specific items
 require("./pages/page-show");
