acfe46dec69a3aad746177782ac35d68aee1c28e
Yoginth <yoginth@aol.com>
2018-01-24 13:51:57 +0000

    Updated Header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -7,7 +7,7 @@
     <link rel="assets" href="https://cdnjs.cloudflare.com">
     <link rel="canonical" href="{{ baseUrl('/') }}">
     <link rel="search" type="application/opensearchdescription+xml" href="/opensearch.xml" title="DocsPen">
-    
+
     <meta name="viewport" content="width=device-width">
     <meta name="token" content="{{ csrf_token() }}">
     <meta name="base-url" content="{{ baseUrl('/') }}">
@@ -15,7 +15,7 @@
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="description" content="DocsPen - Online Documentation Platform. Read more, know more.">
     <meta name="keywords" content="DocsPen, Docs, Documentation, Project Documentation, Wiki">
-    
+
     <link rel="icon" type="image/x-icon" href="/favicon.ico" />
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" sizes="16x16 24x24 32x32 64x64"/>
     <link rel="stylesheet" href="{{ hashed_asset('css/styles.css') }}">
