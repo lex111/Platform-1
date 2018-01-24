9db4c33bcaad2ad48b8cf0ef257e5aa2c8e2c1fa
Yoginth <yoginth@aol.com>
2018-01-24 15:39:26 +0000

    Added ASCII DocsPen

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,3 +1,16 @@
+    <!--
+    
+        8888888b.                             8888888b.                   
+        888  "Y88b                            888   Y88b                  
+        888    888                            888    888                  
+        888    888  .d88b.   .d8888b .d8888b  888   d88P .d88b.  88888b.  
+        888    888 d88""88b d88P"    88K      8888888P" d8P  Y8b 888 "88b 
+        888    888 888  888 888      "Y8888b. 888       88888888 888  888 
+        888  .d88P Y88..88P Y88b.         X88 888       Y8b.     888  888 
+        8888888P"   "Y88P"   "Y8888P  88888P' 888        "Y8888  888  888 
+
+    -->
+    
     <meta charset="utf-8">
     <link rel="dns-prefetch" href="https://docspen.ga">
     <link rel="dns-prefetch" href="https://unpkg.com">
