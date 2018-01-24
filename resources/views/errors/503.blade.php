0fcf55b2a4e1c504a48dd170b9f8e56b4833f2e2
Yoginth <yoginth@aol.com>
2018-01-24 22:18:14 +0530

    Updated 500 by reducing <center> tags

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -5,9 +5,11 @@
     <div class="container small">
         <div class="card white-bg">
             <div class="body">
-                <center><img src="https://unpkg.com/docspen@4.0.0/imgs/unicorn.png" style="height:12em;pointer-events:none"></center>
-                <center><h5 style="color:#FFFFFF"><b>We're having a really bad day.</b></h5></center>
-                <center><p><h5 style="color:#c3c3c3">The Unicorns have taken over. We're doing our best to get them under control and get DocsPen back up and running.</h5></p></center>
+	            <center>
+	            	<img src="https://unpkg.com/docspen@4.0.0/imgs/unicorn.png" style="height:12em;pointer-events:none">
+	                <h5 style="color:#FFFFFF"><b>We're having a really bad day.</b></h5>
+	                <p><h5 style="color:#c3c3c3">The Unicorns have taken over. We're doing our best to get them under control and get DocsPen back up and running.</h5></p>
+	            </center>
             </div>
         </div>
     </div>
