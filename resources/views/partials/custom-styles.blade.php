d1a14b58779b4c9e7378cfedbd2841f83b646625
Yoginth <yoginth@aol.com>
2018-01-24 04:41:45 +0000

    Updated Colors

--- resources/views/partials/custom-styles.blade.php
+++ resources/views/partials/custom-styles.blade.php
@@ -1,6 +1,6 @@
 <style id="custom-styles" data-color="{{ setting('app-color') }}" data-color-light="{{ setting('app-color-light') }}">
     header, [back-to-top], .primary-background { background-color: {{ setting('app-color') }} !important }
-    .faded-small, .primary-background-light { background-color: #e3dbff }
+    .faded-small, .primary-background-light { background-color: #e6e6e6 }
     .button-base, .button, input[type="button"], input[type="submit"] { background-color: {{ setting('app-color') }}; border-color: {{ setting('app-color') }} }
     .button-base:hover, .button:hover, input[type="button"]:hover, input[type="submit"]:hover, .button:focus { background-color: {{ setting('app-color') }} }
     .text-primary, p.primary, p .primary, span.primary:hover, .text-primary:hover, a, a:hover, a:focus, .text-button, .text-button:hover, .text-button:focus { color: {{ setting('app-color') }} }
