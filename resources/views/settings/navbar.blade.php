6b1586eb2fec4a219ecdff91439e37ceb541f572
Yoginth <yoginth@aol.com>
2018-01-24 07:39:35 +0000

    Updated Home

--- resources/views/settings/navbar.blade.php
+++
@@ -1,7 +0,0 @@
-@if($currentUser->can('user-manage') || $currentUser->can('settings-manage') || $currentUser->can('user-roles-manage'))
-<div class="col-md-12 setting-nav nav-tabs">
-    @if($currentUser->can('settings-manage'))
-        <a href="{{ baseUrl('/settings') }}" @if($selected == 'settings') class="selected text-button" @endif><i class="zmdi zmdi-settings"></i>{{ trans('settings.settings') }}</a>
-    @endif
-</div>
-@endif
